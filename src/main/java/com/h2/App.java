f5d18428e18229ae4400f1249515694e1b3ab590
vppflavin <mnzflavio14@hotmail.com>
2021-04-22 14:58:02 -0300

    Atualizei

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -4,14 +4,21 @@ package com.h2;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return 2 * number;
+    }
+
+    private static int add(int[] numbers) {
+        var sun = 0;
+        for (int number : numbers) {
+            sun += number;
+        }
+        return sun;
     }
 }
+
