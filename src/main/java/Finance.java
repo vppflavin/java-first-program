4594a1bb3ab547869ca46e9748f73bcd543a4294
vppflavin <mnzflavio14@hotmail.com>
2021-04-23 17:11:18 -0300

    Finances2.0

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,21 +1,22 @@
-package com.h2;
+import com.h2.BestLoanRates;
+import com.h2.MortgageCalculator;
+import com.h2.SavingsCalculator;
 
 import java.util.Arrays;
 import java.util.Map;
 
 public class Finance {
-    public static final  String BEST_LOAN_RATES = "bestLoanRates";
-    public static final  String SAVINGS_CALCULATOR = "savingsCalculator";
-    public static final  String MORTGAGE_CALCULATOR = "mortgageCalculator";
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR  = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
     public static final Map<String, String> commandsToUsage = Map.of(
             BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
-
-    private static boolean validateCommandArguments(String[] args) {
-        switch (args[0]) {
+    private static boolean validateCommandArguments(String[] args){
+        switch (args[0]){
             case BEST_LOAN_RATES:
                 return args.length == 1;
             case SAVINGS_CALCULATOR:
@@ -24,18 +25,20 @@ public class Finance {
                 return args.length == 4;
         }
         return false;
-    }
 
-    private static void executeCommand(String command, String[] arguments) {
-        switch (command) {
+    }
+    private static void executeCommand (String command, String[] arguments){
+        switch (command){
             case BEST_LOAN_RATES:
                 System.out.println("Finding best loan rates ...");
                 BestLoanRates.main(arguments);
                 return;
+
             case SAVINGS_CALCULATOR:
                 System.out.println("Finding your net savings ...");
                 SavingsCalculator.main(arguments);
                 return;
+
             case MORTGAGE_CALCULATOR:
                 System.out.println("Finding your monthly payment ...");
                 MortgageCalculator.main(arguments);
@@ -45,13 +48,13 @@ public class Finance {
 
     public static void main(String[] args) {
         String command = args[0];
-        if (!commandsToUsage.containsKey(command)) {
-            System.out.println(command +  ": command not found ");
+        if (!commandsToUsage.containsKey(command)){
+            System.out.println(command + ": command not found");
             return;
         }
 
-        boolean isValidcommand = validateCommandArguments(args);
-        if (!isValidcommand) {
+        boolean isValidCommand = validateCommandArguments(args);
+        if (!isValidCommand){
             System.out.println(commandsToUsage.get(args[0]));
             return;
         }
