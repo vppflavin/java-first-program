4fe999bdfe8ce1b9e50e14024857c99c15a748b9
vppflavin <mnzflavio14@hotmail.com>
2021-04-23 14:25:57 -0300

    Utilities corrigido

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -6,7 +6,7 @@ public class Utilities {
         try {
             out = Long.parseLong(in);
         }  catch (NumberFormatException e) {
-            throw new IllegalArgumentException(in + " cannot be converted into a long value. Exiting program.");
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program.");
         }
         return out;
     }
@@ -16,7 +16,7 @@ public class Utilities {
         try {
             out = Integer.parseInt(in);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(in + " cannot be converted into a long value. Exiting program.");
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
         }
         return out;
     }
@@ -26,7 +26,7 @@ public class Utilities {
         try {
             out = Float.parseFloat(in);
         }catch (NumberFormatException e) {
-            throw new IllegalArgumentException(in + " cannot be converted into a long value. Exiting program.");
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
         }
         return out;
 
